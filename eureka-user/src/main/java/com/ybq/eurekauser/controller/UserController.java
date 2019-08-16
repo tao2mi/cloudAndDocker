698034af9302b35c7936c093213bee083f21673a
nickName <876613910@qq.com>
2019-08-17 01:50:26 +0800

    aaa

---
+++ eureka-user/src/main/java/com/ybq/eurekauser/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.ybq.eurekauser.controller;
+
+
+import com.ybq.eurekauser.dao.UserRepository;
+import com.ybq.eurekauser.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        return userRepository.getOne(id);
+    }
+
+}
