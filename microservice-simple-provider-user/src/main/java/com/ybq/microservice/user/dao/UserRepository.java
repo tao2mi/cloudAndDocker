c21ada7ab4f09ebdfec184f73cd2536f68cf7a4d
nickName <876613910@qq.com>
2019-08-17 01:49:57 +0800

    aaa

--- microservice-simple-provider-user/src/main/java/com/ybq/microservice/user/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ybq.microservice.user.dao;
-
-import com.ybq.microservice.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
